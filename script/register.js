d39466777eebe20d71872bf262d0b730c5ca44ee
Kalven21 <zamorakevin21@gmail.com>
2024-03-09 09:13:07 -0800

    registation

--- script/register.js
+++ script/register.js
@@ -11,12 +11,16 @@ let petSalon = {
 let inputName;
 let inputAge;
 let inputGender;
+let inputBreed;
+let inputService;
 
 //creating the constructor
-function Pet(name, age, gender){
+function Pet(name,age,gender,breed,service){
     this.name = name;
     this.age = age;
     this.gender = gender;
+    this.breed = breed;
+    this.service = service;
 }
 /*function calculateAverage(){
     let totalAge = 0;
@@ -29,9 +33,9 @@ function Pet(name, age, gender){
 }*/
 function register(){
     //get the values from the HTML
-    console.log(inputName.value,inputAge.value,inputGender.value);
+    console.log(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value);
     //create the object using the constructor
-    let newPet = new Pet(inputName.value,inputAge.value,inputGender.value);
+    let newPet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value);
     //spush the objet to the array
     petSalon.pets.push(newPet);
     //display the array on the console
@@ -41,24 +45,26 @@ function isValid(){
 
 }
 function init(){
-    
     //hook events
-
     //initial functions
     //calculateAverage();
-    
     //creating a new pet
-    let pet1 = new Pet("Scooby",40,"Male");
+    let pet1 = new Pet("Scooby",40,"Male","Dog","");
     petSalon.pets.push(pet1);
+    let pet2 = new Pet("Doggy",60,"Male","Dog","");
+    petSalon.pets.push(pet2);
+    let pet3 = new Pet("Nia",20,"Female","Dog","");
+    petSalon.pets.push(pet3);
+    let pet4 = new Pet("Chato",60,"Male","Cat","");
+    petSalon.pets.push(pet4);
     displayNames();
-    let pet2 = new Pet("Doggy",60,"Male");
-    let pet3 = new Pet("Nia",20,"Female");
-    let pet4 = new Pet("Chato",60,"Male");
 
     //load the inputs
     inputName = document.getElementById("txtName");
     inputAge = document.getElementById("txtAge");
     inputGender = document.getElementById("txtGender");
+    inputGender = document.getElementById("txtBreed");
+    inputGender = document.getElementById("txtService");
 }
 
 window.onload=init;
